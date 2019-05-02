6fa53d564df86e792832feefb43e85f14adf9822
Kimberly McConnell <kmcconnell393@gmail.com>
2019-05-02 10:51:13 -0700

    create firebase functions deploying to firebase functions

---
+++ app.js
@@ -0,0 +1,17 @@
+// const functions = require("firbase-functions")
+// const express = require("express")
+
+
+// // Express 
+
+// const app = express();
+// app.get("*", (req, res) => {
+//   res.send("Hellow from Express on Firebase?")
+// })
+
+// const api = functions.https.onRequest(app)
+
+// module.exports = {
+//   api
+// }
+
